7d8f85384f84594f463a2fcaf00493c0edabed65
Athanasios Sotiroglou <thanasissot1@gmail.com>
2025-10-12 11:27:56 +0300

    added logging with log4j2

--- src/main/java/com/asot/springbootdemo/controller/TestEntityController.java
+++ src/main/java/com/asot/springbootdemo/controller/TestEntityController.java
@@ -5,6 +5,7 @@ import com.asot.springbootdemo.model.TestEntity;
 import com.asot.springbootdemo.service.TestEntityService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,16 +13,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/test-entities")
 @RequiredArgsConstructor
+@Log4j2
 public class TestEntityController {
     private final TestEntityService testEntityService;
 
     @GetMapping
     public List<TestEntityDTO> getAllTestEntities() {
+        log.info("getAllTestEntities request received.");
         return testEntityService.getAllTestEntities();
     }
 
     @PostMapping
     public TestEntityDTO createTestEntity(@RequestBody @Valid TestEntityDTO testEntityDTO) {
+        log.info("createTestEntity request received.");
         return testEntityService.saveTestEntity(testEntityDTO);
     }
 
